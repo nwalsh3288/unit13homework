140dac07dcd215773d4fb6ccb0429844444890a0
Nicole Walsh <nmwalsh92@gmail.com>
2020-09-02 19:29:15 -0700

    unit13homework

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger",TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie",TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BLT",FALSE);
